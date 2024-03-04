6ed6ee2fddae50982fe613ff7a7193b25eb65f20
Miracle Apata <prmpsmart@gmail.com>
2024-03-04 18:39:56 +0100

    started the implementation

---
+++ activity_calendar_widget.py
@@ -0,0 +1,6 @@
+from PySide6.QtWidgets import QWidget, QPushButton
+
+
+class ActivityCalendarWidget(QWidget):
+    def __init__(self, parent: QWidget):
+        super().__init__(parent)
---
+++ main.py
@@ -0,0 +1,3 @@
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton
+from activity_calendar_widget import ActivityCalendarWidget
+
